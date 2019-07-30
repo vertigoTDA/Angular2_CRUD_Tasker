3165e9aae6b97a61f9fc470d038b16f8bebd74a6
VertigoTDA <tsybulkodmytro@gmail.com>
2019-07-30 10:41:33 -0700

    add models/task.model.ts

---
+++ src/app/models/task.model.ts
@@ -0,0 +1,12 @@
+export class Task {
+    id: number;
+    status: string;
+    date: Date;
+    deadEnd: Date;
+    creator: string;
+    executor: string;
+    executorPhotoPath: string;
+    title: string;
+    taskBody: string;
+    notes: string;
+}
