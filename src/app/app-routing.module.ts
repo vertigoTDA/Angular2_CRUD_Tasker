655f4adca9ab3834cc0c95ea6d9280f0de74733d
VertigoTDA <git@github.com:vertigoTDA>
2019-08-19 14:56:50 +0300

    updating list-taskComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './tasks/home.component';
 import { ListTasksComponent } from './tasks/list-tasks.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CreateTaskComponent } from './tasks/create-task.component';
+import { RecycleComponent } from './tasks/recycle.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'list', component: ListTasksComponent },
+  { path: 'newTask', component: CreateTaskComponent },
+  { path: 'recycle', component: RecycleComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
