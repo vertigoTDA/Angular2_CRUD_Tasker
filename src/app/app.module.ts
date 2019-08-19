acaad910487b0f51baa9bf3ce3c44a2800cbdd5d
VertigoTDA <git@github.com:vertigoTDA>
2019-08-19 13:32:04 +0300

    creating newTaskComponent + RecycleComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { ListTasksComponent } from './tasks/list-tasks.component';
 import { HomeComponent } from './tasks/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CreateTaskComponent } from './tasks/create-task.component';
+import { RecycleComponent } from './tasks/recycle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListTasksComponent,
     HomeComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    CreateTaskComponent,
+    RecycleComponent
   ],
   imports: [
     BrowserModule,
